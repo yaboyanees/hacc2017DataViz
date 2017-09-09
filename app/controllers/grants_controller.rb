8336e31812deefd4d7631ac97ba8c9454a079ec5
Noah Higa <higa.noah@gmail.com>
2017-09-08 16:13:02 -1000

    Added toggle to stacked chart.

--- app/controllers/grants_controller.rb
+++ app/controllers/grants_controller.rb
@@ -23,10 +23,12 @@ class GrantsController < ApplicationController
       format.json { head :no_content }
     end
   end
-    
+	
+	
   # GET /grants
   # GET /grants.json
   def index
+	preferredY = "amount"
     @grants = Grant.all
     @grant_count = @grants.count
     @year_count = @grants.group(:fiscal_year).order(:fiscal_year).count
@@ -36,52 +38,53 @@ class GrantsController < ApplicationController
 	@location_count = @grants.group(:location).order(:location).count
 	
 	#These are arrays of the unique values for each parameter. Unique locations, unique fiscal years...
-	@uLocations = @grants.select(:location).distinct.pluck(:location)	
-	@uYears = @grants.select(:fiscal_year).distinct.order(:fiscal_year).pluck(:fiscal_year)
-	@uPriorities = @grants.select(:strategic_priority).distinct.pluck(:strategic_priority)
-	@uResults = @grants.select(:strategic_results).distinct.pluck(:strategic_results)
+	@uLocations = @grants.distinct.pluck(:location)	
+	@uYears = @grants.distinct.order(:fiscal_year).pluck(:fiscal_year)
+	@uPriorities = @grants.distinct.pluck(:strategic_priority)
+	@uResults = @grants.distinct.pluck(:strategic_results)
 	
 	#Total funding, grouped by location
 	@sumLocations = Array.new
 	@uLocations.each do |loc|
-		@sumLocations.push(@grants.where(location: loc).sum("amount").to_f)
+		@sumLocations.push(@grants.where(location: loc).sum(preferredY).to_f)
 	end
 		
 	#Total funding, grouped by strategic priority
 	@sumPriorities = Array.new
 	@uPriorities.each do |pri|
-		@sumPriorities.push(@grants.where(strategic_priority: pri).sum("amount").to_f)
+		@sumPriorities.push(@grants.where(strategic_priority: pri).sum(preferredY).to_f)
 	end
 	
 	#total funding, grouped by strategic results
 	@sumResults = Array.new
 	@uResults.each do |res|
-		@sumResults.push(@grants.where(strategic_results: res).sum("amount").to_f)
+		@sumResults.push(@grants.where(strategic_results: res).sum(preferredY).to_f)
 	end
 	
-	#This is a 2-d array of funding for each island, for each year.
+	#This is a 2-d array of the preferred measurement for each island, for each year.
 	#Inner array is each year. Outer array is location.
 	#For example, [0][0] would be a location in 2013, but [0][1] is 2014.
 	@amountPerYearByLocation = Array.new
 	@uLocations.each do |loc|
 			@locArray = Array.new
 			@uYears.each do |year|
-				@locArray.push(@grants.where(location: loc).where(fiscal_year: year).sum("amount").to_f)
+				@locArray.push(@grants.where(location: loc).where(fiscal_year: year).sum(preferredY).to_f)
 			end
 			@amountPerYearByLocation.push(@locArray)
 	end
 	
-	#This is a 2-d array of funding for each strategic priority, for each year.
+	#This is a 2-d array of the preferred measurement for each strategic priority, for each year.
 	#Inner array is each year. Outer array is location.
 	#For example, [0][0] would be a priority in 2013, but [0][1] is 2014.
 	@amountPerYearByPriority = Array.new
 	@uPriorities.each do |pri|
 			@priArray = Array.new
 			@uYears.each do |year|
-				@priArray.push(@grants.where(strategic_priority: pri).where(fiscal_year: year).sum("amount").to_f)
+				@priArray.push(@grants.where(strategic_priority: pri).where(fiscal_year: year).sum(preferredY).to_f)
 			end
 			@amountPerYearByPriority.push(@priArray)
 	end
+	
 
 	
 	
